b763433a139970f049284a9ff1c485a5e4b452ad
Shirley Leung <sleung@idmod.org>
2021-09-21 23:26:32 -0700

    save out small csv w/ more inset chart columns for spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne

--- save_out_small_csvs.py
+++ save_out_small_csvs.py
@@ -56,16 +56,24 @@ import pandas as pd
 
 # -- spatial, classic, GM only, EIR = 30
 # NOTE THAT THE 6 SWEEP VAR WORK ITEM DOESN'T HAVE ALLELE FREQS (EXP DOES)
-# wi_name = 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne'
-# wi_name_sh = 'spatial, classic drive, GM only, EIR = 30'
-# wi_names = ['spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_release_day_release_node_num',
-#             'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_newrr0',
-#             'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_newsne']
+wi_name = 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne'
+wi_name_sh = 'spatial, classic drive, GM only, EIR = 30'
+wi_names = [
+    # 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_release_day_release_node_num',
+    'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne',
+    'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_newrr0',
+    'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne_newsne'
+]
+# BEFORE ADDING IN MORE INSET CHART VARS
 # data_dirs = ['Z:\\home\\sleung\\workitems\\648\\d61\\287\\648d6128-78f9-eb11-a9ed-b88303911bc1',
 #              'Y:\\home\\sleung\\workitems\\89d\\46b\\9f9\\89d46b9f-950b-ec11-a9ed-b88303911bc1',
 #              'Y:\\home\\sleung\\workitems\\279\\e5f\\038\\279e5f03-8b12-ec11-a9ed-b88303911bc1']
-# num_sweep_vars = 6
-# drive_type = 'classic'
+# AFTER ADDING IN MORE INSET CHART VARS AND RE-RUNNING THE 1ST EXP W/ 4 VARS
+data_dirs = ['Y:\\home\\sleung\\workitems\\389\\e58\\cb0\\389e58cb-001b-ec11-a9ed-b88303911bc1',
+             'Y:\\home\\sleung\\workitems\\244\\8a9\\816\\2448a981-6e1a-ec11-a9ed-b88303911bc1',
+             'Y:\\home\\sleung\\workitems\\7c4\\563\\fc6\\7c4563fc-6e1a-ec11-a9ed-b88303911bc1']
+num_sweep_vars = 4  # 6
+drive_type = 'classic'
 
 # -- spatial, integral, GM only, EIR = 30
 # wi_name = 'spatialinside_integral2l4a_GM_only_aEIR30_sweep_rc_d1_rr20_se2'
@@ -80,21 +88,21 @@ import pandas as pd
 # drive_type = 'integral'
 
 # -- spatial, classic, VC and GM, EIR = 30
-wi_name = 'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne'
-wi_name_sh = 'spatial, classic drive, VC and GM, EIR = 30'
-wi_names = ['spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne',
-            'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne_newrr0',
-            'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne_newsne']
-# BEFORE ADDING IN MORE INSET CHART VARS
-# data_dirs = ['Y:\\home\\sleung\\workitems\\a82\\f7d\\335\\a82f7d33-5705-ec11-a9ed-b88303911bc1',
-#              'Y:\\home\\sleung\\workitems\\8b9\\2f1\\679\\8b92f167-950b-ec11-a9ed-b88303911bc1',
-#              'Y:\\home\\sleung\\workitems\\aba\\769\\f07\\aba769f0-7312-ec11-a9ed-b88303911bc1']
-# AFTER ADDING IN MORE INSET CHART VARS
-data_dirs = ['Y:\\home\\sleung\\workitems\\a46\\048\\996\\a4604899-6f1a-ec11-a9ed-b88303911bc1',
-             'Y:\\home\\sleung\\workitems\\24c\\b36\\fc6\\24cb36fc-6f1a-ec11-a9ed-b88303911bc1',
-             'Y:\\home\\sleung\\workitems\\f67\\e3c\\d56\\f67e3cd5-6f1a-ec11-a9ed-b88303911bc1']
-num_sweep_vars = 4
-drive_type = 'classic'
+# wi_name = 'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne'
+# wi_name_sh = 'spatial, classic drive, VC and GM, EIR = 30'
+# wi_names = ['spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne',
+#             'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne_newrr0',
+#             'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne_newsne']
+# # BEFORE ADDING IN MORE INSET CHART VARS
+# # data_dirs = ['Y:\\home\\sleung\\workitems\\a82\\f7d\\335\\a82f7d33-5705-ec11-a9ed-b88303911bc1',
+# #              'Y:\\home\\sleung\\workitems\\8b9\\2f1\\679\\8b92f167-950b-ec11-a9ed-b88303911bc1',
+# #              'Y:\\home\\sleung\\workitems\\aba\\769\\f07\\aba769f0-7312-ec11-a9ed-b88303911bc1']
+# # AFTER ADDING IN MORE INSET CHART VARS
+# data_dirs = ['Y:\\home\\sleung\\workitems\\a46\\048\\996\\a4604899-6f1a-ec11-a9ed-b88303911bc1',
+#              'Y:\\home\\sleung\\workitems\\24c\\b36\\fc6\\24cb36fc-6f1a-ec11-a9ed-b88303911bc1',
+#              'Y:\\home\\sleung\\workitems\\f67\\e3c\\d56\\f67e3cd5-6f1a-ec11-a9ed-b88303911bc1']
+# num_sweep_vars = 4
+# drive_type = 'classic'
 
 # -- spatial, integral, VC and GM, EIR = 30
 # wi_name = 'spatialinside_integral2l4a_VC_and_GM_aEIR30_sweep_rc_d1_rr20_se2'
@@ -151,41 +159,43 @@ if (wi_name == 'spatialinside_integral2l4a_VC_and_GM_aEIR30_sweep_rc_d1_rr20_se2
         file_suffixes[0].append(fsbegtemp)
     # - 2nd and 3rd work items have no partition vars
 elif (wi_name == 'spatialinside_classic3allele_VC_and_GM_aEIR30_sweep_rc_d_rr0_sne') \
-        or (wi_name == 'spatialinside_classic3allele_VC_and_GM_aEIR10_sweep_rc_d_rr0_sne'):
+        or (wi_name == 'spatialinside_classic3allele_VC_and_GM_aEIR10_sweep_rc_d_rr0_sne') \
+        or (wi_name == 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne') \
+        or (wi_name == 'spatialinside_classic3allele_GM_only_aEIR10_sweep_rc_d_rr0_sne'):
     # - 1st, 2nd, and 3rd work items have no partition vars
     for i in range(3):
         file_suffixes.append([])
-elif (wi_name == 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne') \
-        or (wi_name == 'spatialinside_classic3allele_GM_only_aEIR10_sweep_rc_d_rr0_sne'):
-    for i in range(3):
-        file_suffixes.append([])
-    # - 1st work item
-    partition_vars = ['num_nodes', 'd', 'rr0']
-    partition_vars_vals = [[12, 6], [1, 0.95, 0.9], [0, 0.1, 0.2]]
-    for partition_vars_val0 in partition_vars_vals[0]:
-        fsbegtemp = partition_vars[0] + str(partition_vars_val0)
-        for partition_vars_val1 in partition_vars_vals[1]:
-            fsmidtemp = partition_vars[1] + str(partition_vars_val1)
-            for partition_vars_val2 in partition_vars_vals[2]:
-                fsendtemp = partition_vars[2] + str(partition_vars_val2)
-                file_suffixes[0].append(fsbegtemp + '_' + fsmidtemp + '_' + fsendtemp)
-    # - 2nd and 3rd work items have no partition vars
-elif wi_name == 'spatialinside_classic3allele_VC_and_GM_aEIR80_sweep_rc_d_rr0_sne':
-    for i in range(3):
-        file_suffixes.append([])
-    # - 1st work item
-    partition_vars = ['num_nodes', 'd', 'rr0', 'sne']
-    partition_vars_vals = [[12, 6], [1, 0.95, 0.9], [0, 0.1, 0.2], [0, 0.05, 0.1, 0.15, 0.2]]
-    for partition_vars_val0 in partition_vars_vals[0]:
-        fsbegtemp = partition_vars[0] + str(partition_vars_val0)
-        for partition_vars_val1 in partition_vars_vals[1]:
-            fsmidtemp = partition_vars[1] + str(partition_vars_val1)
-            for partition_vars_val2 in partition_vars_vals[2]:
-                fsendtemp = partition_vars[2] + str(partition_vars_val2)
-                for partition_vars_val3 in partition_vars_vals[3]:
-                    fsend1temp = partition_vars[3] + str(partition_vars_val3)
-                    file_suffixes[0].append(fsbegtemp + '_' + fsmidtemp + '_' + fsendtemp + '_' + fsend1temp)
-    # - 2nd and 3rd work items have no partition vars
+# elif (wi_name == 'spatialinside_classic3allele_GM_only_aEIR30_sweep_rc_d_rr0_sne') \
+#         or (wi_name == 'spatialinside_classic3allele_GM_only_aEIR10_sweep_rc_d_rr0_sne'):
+#     for i in range(3):
+#         file_suffixes.append([])
+#     # - 1st work item
+#     partition_vars = ['num_nodes', 'd', 'rr0']
+#     partition_vars_vals = [[12, 6], [1, 0.95, 0.9], [0, 0.1, 0.2]]
+#     for partition_vars_val0 in partition_vars_vals[0]:
+#         fsbegtemp = partition_vars[0] + str(partition_vars_val0)
+#         for partition_vars_val1 in partition_vars_vals[1]:
+#             fsmidtemp = partition_vars[1] + str(partition_vars_val1)
+#             for partition_vars_val2 in partition_vars_vals[2]:
+#                 fsendtemp = partition_vars[2] + str(partition_vars_val2)
+#                 file_suffixes[0].append(fsbegtemp + '_' + fsmidtemp + '_' + fsendtemp)
+#     # - 2nd and 3rd work items have no partition vars
+# elif wi_name == 'spatialinside_classic3allele_VC_and_GM_aEIR80_sweep_rc_d_rr0_sne':
+#     for i in range(3):
+#         file_suffixes.append([])
+#     # - 1st work item
+#     partition_vars = ['num_nodes', 'd', 'rr0', 'sne']
+#     partition_vars_vals = [[12, 6], [1, 0.95, 0.9], [0, 0.1, 0.2], [0, 0.05, 0.1, 0.15, 0.2]]
+#     for partition_vars_val0 in partition_vars_vals[0]:
+#         fsbegtemp = partition_vars[0] + str(partition_vars_val0)
+#         for partition_vars_val1 in partition_vars_vals[1]:
+#             fsmidtemp = partition_vars[1] + str(partition_vars_val1)
+#             for partition_vars_val2 in partition_vars_vals[2]:
+#                 fsendtemp = partition_vars[2] + str(partition_vars_val2)
+#                 for partition_vars_val3 in partition_vars_vals[3]:
+#                     fsend1temp = partition_vars[3] + str(partition_vars_val3)
+#                     file_suffixes[0].append(fsbegtemp + '_' + fsmidtemp + '_' + fsendtemp + '_' + fsend1temp)
+#     # - 2nd and 3rd work items have no partition vars
 
 # -------- Load data
 dfi = pd.DataFrame()
@@ -202,7 +212,8 @@ for iwn in range(len(file_suffixes)):
             filea = os.path.join(data_dirnow, wi_namenow + '_spatial_avg_allele_freqs_' + file_suffix + '.csv')
             filee = os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_'
                                  + str(elim_day) + '_indiv_sims_' + file_suffix + '.csv')
-            fileed = os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_number_indiv_sims_' + file_suffix + '.csv')
+            fileed = os.path.join(data_dirnow,
+                                  wi_namenow + '_inset_data_elim_day_number_indiv_sims_' + file_suffix + '.csv')
             dfi = dfi.append(pd.read_csv(filei))
             dfa = dfa.append(pd.read_csv(filea))
             dfe = dfe.append(pd.read_csv(filee))
@@ -210,8 +221,10 @@ for iwn in range(len(file_suffixes)):
     else:
         dfi = dfi.append(pd.read_csv(os.path.join(data_dirnow, wi_namenow + '_inset_data.csv')))
         dfa = dfa.append(pd.read_csv(os.path.join(data_dirnow, wi_namenow + '_spatial_avg_allele_freqs.csv')))
-        dfe = dfe.append(pd.read_csv(os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_' + str(elim_day) + '_indiv_sims.csv')))
-        dfed = dfed.append(pd.read_csv(os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_number_indiv_sims.csv')))
+        dfe = dfe.append(pd.read_csv(
+            os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_' + str(elim_day) + '_indiv_sims.csv')))
+        dfed = dfed.append(
+            pd.read_csv(os.path.join(data_dirnow, wi_namenow + '_inset_data_elim_day_number_indiv_sims.csv')))
 
 # -------- Clean up data
 dfe = dfe.drop(columns=['Daily_EIR_elim', 'New_Clinical_Cases_elim', 'Run_Number'])
