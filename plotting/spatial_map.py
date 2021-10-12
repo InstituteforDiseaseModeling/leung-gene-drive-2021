fa73f1b228ba878d9dcf8d1e430dc8b3e50ebf61
Shirley Leung <sleung@idmod.org>
2021-10-12 15:31:21 -0700

    plot spatial map, fig 1

---
+++ plotting/spatial_map.py
@@ -0,0 +1,135 @@
+import json
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+from matplotlib import rcParams
+import numpy as np
+import os
+import pandas as pd
+import shapefile
+
+rcParams['pdf.fonttype'] = 42
+# rcParams['font.family'] = 'Myriad Pro'
+# rcParams.update({'font.size': 21})
+# rcParams.update({'font.size': 12})
+
+# ------ Set fig path
+fig_dir = 'C:\\Users\\sleung\\OneDrive - Institute for Disease Modeling\\presentation_writeups\\gene_drive_paper\\figures'
+os.makedirs(fig_dir, exist_ok=True)
+
+##
+# ------ Load grid/demographics data
+demographics_dir = 'C:\\Users\\sleung\\OneDrive - Institute for Disease Modeling\\' \
+                   'DTK_input_output_staging\\input\\BurkinaFasoSpatialInsideNodes\\Demographics\\'
+demographics_file = 'demographics.json'
+demographics_filepath = os.path.join(demographics_dir, demographics_file)
+nodes = []
+pop = []
+lat = []
+lon = []
+with open(demographics_filepath) as f:
+    data = json.load(f)
+    for node in data['Nodes']:
+        nodes.append(node['NodeID'])
+        pop.append(node['NodeAttributes']['InitialPopulation'])
+        lat.append(node['NodeAttributes']['Latitude'])
+        lon.append(node['NodeAttributes']['Longitude'])
+dfg = pd.DataFrame({'NodeID': nodes, 'pop': pop, 'lat': lat, 'lon': lon})
+
+
+##
+# ------ Load spatial plot shapefiles + set map boundaries
+
+# - Set map boundaries
+lngmin = min(dfg['lon']) - 0.02
+lngmax = max(dfg['lon']) + 0.02
+latmin = min(dfg['lat']) - 0.02
+latmax = max(dfg['lat']) + 0.02
+minmaxes = (lngmin, lngmax, latmin, latmax)
+
+
+# - Load roads and rivers
+def features_within_polygon(shapefile_path, lngmin, lngmax, latmin, latmax):
+    """
+    Plot man-made features like roads or natural features like rivers
+    :param shapefile_path: path to shapefile describing feature
+    :param lngmin: westernmost longitude of polygon to plot
+    :param lngmax: easternmost longitude of polygon to plot
+    :param latmin: southernmost longitude of polygon to plot
+    :param latmax: northernmost longitude of polygon to plot
+    :return: features: coordinates of features
+    """
+
+    sf = shapefile.Reader(shapefile_path)
+    features = []
+    for shape in sf.shapeRecords():
+        x = [i[0] for i in shape.shape.points[:]]
+        x2 = [i for i in x if lngmin < i < lngmax]
+        y = [i[1] for i in shape.shape.points[:]]
+        y2 = [i for i in y if latmin < i < latmax]
+        if x2 and y2:
+            features.append(shape.shape.points[:])
+
+    return features
+
+
+river_shapefile_path = os.path.join(os.path.expanduser('~'), 'Dropbox (IDM)',
+                                    'Malaria Team Folder', 'data',
+                                    'Burkina', 'Burkina shapefiles', 'BFA_wat', 'BFA_water_lines_dcw')
+road_shapefile_path = os.path.join(os.path.expanduser('~'), 'Dropbox (IDM)',
+                                   'Malaria Team Folder', 'data',
+                                   'Burkina', 'Burkina shapefiles', 'BFA_rds', 'BFA_roads')
+rivers = features_within_polygon(river_shapefile_path, lngmin=lngmin, lngmax=lngmax, latmax=latmax, latmin=latmin)
+roads = features_within_polygon(road_shapefile_path, lngmin=lngmin, lngmax=lngmax, latmax=latmax, latmin=latmin)
+
+##
+# ------ Plot population on spatial grid
+
+# - Set color map
+cmap = cm.plasma
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+
+# - Plot rivers
+for river in rivers:
+    x = [i[0] for i in river]
+    y = [i[1] for i in river]
+    ax.plot(x, y, zorder=1, color='#7faddd', lw=5)
+
+# - Plot roads
+for road in roads:
+    x = [i[0] for i in road]
+    y = [i[1] for i in road]
+    ax.plot(x, y, zorder=1, color='#969696', lw=3)
+
+ax.set_ylim([minmaxes[2], minmaxes[3]])
+ax.set_xlim([minmaxes[0], minmaxes[1]])
+
+# - Color grid cells by population
+norm = colors.Normalize(vmin=dfg['pop'].min(), vmax=dfg['pop'].max())
+m = cm.ScalarMappable(norm=norm, cmap=cmap)
+dfg['colors'] = dfg['pop'].apply(lambda x: m.to_rgba(x))
+ax.scatter(dfg['lon'], dfg['lat'], zorder=2, marker='s', s=805, c=dfg['colors'], edgecolor='silver')
+
+# - Highlight gene drive release nodes
+
+# - Annotate each grid cell w/ pop numbers
+# for irow in range(0, len(dfg)):
+#     dfnow = dfg.iloc[irow]
+#     ax.text(dfnow['lon'], dfnow['lat'], str(dfnow['pop']), fontsize=10, fontweight='bold', color='silver')
+
+# - Plot scalebar
+ax.plot([-1.57267, -1.52673], [12.02, 12.02], 'k', lw=2)
+ax.text(-1.57267, 12.01, r'5 km', fontsize=21)
+
+# - Make plot square
+ax.set_aspect('equal', adjustable='box')
+
+# - Remove x and y axis labels
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+
+# - Save plot
+fig_file = os.path.join(fig_dir, 'spatialinside_human_pop.png')
+plt.savefig(fig_file, dpi=300)
+plt.show()
